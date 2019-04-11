ea9b8bdeded64b21d274cfd7db21c3851fd84dce
fra <ffrancesco96@hotmail.it>
2019-04-11 19:51:55 +0200

    Commit coda #1

---
+++ coda.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct nodo{
+  char indice[128];
+  struct nodo* suc;
+} nodo;
+
+
+struct nodo* coda;
+
+
+struct nodo* libera_n(struct nodo *n){
+  //nodo e' il puntatore al nodo da liberare
+  //restituisce il puntatore al nuovo nodo della lista (e quindi alla nuova testa)
+   struct nodo* new;
+   new=n->suc;
+   free(n->indice);
+   free(n);
+   return new;
+}
+
+void libera_ln(struct nodo *testa){
+
+  //testa e' il puntatore alla radice della lista collegata
+  //libera la lista dalla memoria
+  struct nodo* aus;
+  while(testa!=NULL){
+    aus=testa->suc;
+    free(testa->indice); 
+    free(testa);
+    testa=aus;
+  }
+}
+
+struct nodo* inserisci_in_coda(struct nodo *coda,struct nodo *mes){
+  //testa e' l' ultimo nodo della lista, mes il nodo da inserire
+  //la funzione aggiorna il puntatore all' ultimo nodo con quello appena inserito
+  if(coda==NULL){
+    coda=mes;
+  }else{
+    coda->suc=mes;
+    coda=mes;
+  }
+  return coda;
+}
+
+
+struct nodo* inserisci_n(struct nodo* testa,struct nodo* nod){
+  struct nodo* aus;
+  struct nodo* aus1;
+  struct nodo* aus2;
+  aus2=testa;
+  aus1=aus2;
+
+  while(testa!=NULL){
+    aus=testa->suc;
+    if(strcmp(testa->indice,nod->indice)==0){
+      if(testa->suc!=NULL){
+          if(testa==aus2){
+              aus2=libera_n(testa);
+          }else{
+              puts(aus1->indice);
+              aus1->suc=libera_n(testa);
+          }   
+          break;
+      }else{
+          return testa;
+      }
+    } 
+    aus1=testa;
+    testa=aus;
+  }
+  coda=inserisci_in_coda(coda,nod);  
+  testa=aus2;
+  return testa;
+}
+
+void stampa(struct nodo *testa){
+  struct nodo* aus;
+  while(testa!=NULL){
+    aus=testa->suc;
+    printf("STAMPA:%s\n",testa->indice);
+    testa=aus;
+  }
+  printf("Coda:%s\n\n",coda->indice);
+}
+
+void main(){
+    struct nodo* testa;
+    testa=NULL;
+    coda=NULL;
+    
+
+    struct nodo *n=malloc(sizeof(struct nodo));
+    strcpy(n->indice,"ind1");
+    n->suc=NULL;
+    testa=n;
+    coda=n;
+
+    stampa(testa);
+    
+    struct nodo *n2=malloc(sizeof(struct nodo));
+    strcpy(n2->indice,"ind2");
+    n2->suc=NULL;
+    testa=inserisci_n(testa,n2);
+    
+    stampa(testa);
+    
+    struct nodo *n3=malloc(sizeof(struct nodo));
+    strcpy(n3->indice,"ind3");
+    n3->suc=NULL;
+    testa=inserisci_n(testa,n3);
+  
+    stampa(testa);
+
+    struct nodo *n4=malloc(sizeof(struct nodo));
+    strcpy(n4->indice,"ind1");
+    n4->suc=NULL;
+    testa=inserisci_n(testa,n4);
+
+    stampa(testa);
+
+    struct nodo *n5=malloc(sizeof(struct nodo));
+    strcpy(n5->indice,"ind4");
+    n5->suc=NULL;
+    testa=inserisci_n(testa,n5);
+
+    stampa(testa);
+
+    printf("%s\n",testa->indice);
+    struct nodo *n6=malloc(sizeof(struct nodo));
+    strcpy(n6->indice,"ind3");
+    n6->suc=NULL;
+    printf("%s\n",n6->indice);
+    
+    //testa=inserisci_n(testa,n6);
+
+    stampa(testa); 
+    
+
+    printf("ASD:%s\n",n->indice);//perche' non da errore??
+    libera_ln(testa);
+    printf("%s\n",testa->indice);//perche' non da errore??
+    
+}
