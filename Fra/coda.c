4b5c5902746ec0a09f3010ac878a24581dcf2c14
fra <ffrancesco96@hotmail.it>
2019-04-27 12:29:23 +0200

    Commit coda#5

--- Fra/coda.c
+++ Fra/coda.c
@@ -3,28 +3,24 @@
 #include <string.h>
 #include "coda.h"
 
-struct nodo{
-  char indice[128]; //identificatore del file nella tabella 
-  struct nodo* suc; //puntatore al nodo successivo della lista collegata(coda)
-} nodo;
-
 struct nodo* coda; //puntatore alla coda della lista collegata
 
-struct nodo* libera_n(struct nodo *testa,char *n){
+struct nodo* libera_n(struct nodo *testa,long n){
   //testa e' il puntatore alla testa della coda,n e' il puntatore al nodo da liberare
   //restituisce il puntatore alla testa
    struct nodo* new;
    struct nodo* aus;
    aus=testa;
-   if(strcmp(testa->indice,n)==0){ //se il nodo da eliminare e' quello in testa 
-      new=testa->suc; //la nuova testa punta al successivo della vecchia
-      free(testa->indice); 
+   if(testa->indice==n){ //se il nodo da eliminare e' quello in testa 
+      extern limite_dimensione;
+      limite_dimensione-=deleteHash(n);//CONTROLLARE perche' la funzione puo' ritornare NULL
+      new=testa->suc; //la nuova testa punta al successivo della vecchia 
       free(testa);
       return new; //ritorno la nuova testa
    }
    else{ //se il nodo da eliminare e' nel mezzo della lista
       
-     while(!(strcmp(testa->suc->indice,n)==0)){ //cerco il nodo nella lista
+     while(!(testa->suc->indice==n)){ //cerco il nodo nella lista
      //testa tiene traccia del nodo precedente al nodo da eliminare
         if(testa->suc->suc==NULL){ //se il nodo testa->suc non e' quello cercato e il nodo successivo e' NULL => il nodo ricercato non e' nella lista
           puts("ERRORE:nodo non presente nella lista");
@@ -36,8 +32,7 @@ struct nodo* libera_n(struct nodo *testa,char *n){
      testa->suc=testa->suc->suc; //il nodo precedente punta al nodo successivo 
      if(testa->suc==NULL){
        coda=testa;
-     }
-     free(testa->suc->indice); 
+     } 
      free(testa->suc);
      return aus; //ritorno la testa(non modificata)
    }
@@ -49,7 +44,6 @@ void libera_ln(struct nodo *testa){
   struct nodo* aus;
   while(testa!=NULL){
     aus=testa->suc;
-    free(testa->indice); 
     free(testa);
     testa=aus;
   }
@@ -68,7 +62,7 @@ struct nodo* inserisci_in_coda(struct nodo *coda,struct nodo *mes){//si potrebbe
   return coda;
 }
 
-struct nodo* inserisci_n(struct nodo* testa,char* nod){
+struct nodo* inserisci_n(struct nodo* testa,long nod){
   struct nodo* aus; //puntatore al nodo successivo della lista
   struct nodo* aus1; //puntatore al nodo precente a quello da ricercare/inserire
   struct nodo* aus2;//puntatore alla testa della lista
@@ -80,7 +74,7 @@ struct nodo* inserisci_n(struct nodo* testa,char* nod){
       puts("Impossibile allocare memoria");
       exit(-1);
     }
-    strcpy(n->indice,nod);
+    n->indice=nod;
     n->suc=NULL;
     testa=n;
     coda=n;
@@ -89,7 +83,7 @@ struct nodo* inserisci_n(struct nodo* testa,char* nod){
   //ricerco nella lista il nodo da inserire
   while(testa!=NULL){
     aus=testa->suc; //tengo traccia del nodo successivo in quanto testa verra' modificato
-    if(strcmp(testa->indice,nod)==0){ //se gli indici dei due nodi corrispondono
+    if(testa->indice==nod){ //se gli indici dei due nodi corrispondono
       if(testa->suc!=NULL){ //e il nodo non si trova gia' in coda(non ha senso sostituirlo se e' gia' in coda)
           //aggiornamento
           if(testa==aus2){  //se il nodo trovato e' in testa=> la nuova testa puntera' al nodo successivo della vecchia
@@ -97,13 +91,7 @@ struct nodo* inserisci_n(struct nodo* testa,char* nod){
           }else{ //se il nodo e' in mezzo alla lista: il precedente punta al successivo
               aus1->suc=testa->suc;
           }   
-
-/*PROBLEMA:
-Quando chiamo questa funzione(inserisci_n) io alloco memoria per il nodo nod a prescindere se gia' l' ho inserito in precedenza o meno nella lista(perche' non riesco a tenere traccia degli indici inseriti in precedenza, cioe' io ogni volta che ho un inserimento creo un nodo, gli alloco la memoria e chiamo questa funzione)
-quindi se lo trovo nella lista, io semplicemente lo ignoro(facendo puntare il precedente al successivo) e poi dico a nod(il nuovo nodo) di diventare coda
-Cosi' facendo pero' ho in memoria due nodi identici (uno dovuto all' inserimento precedente,cioe' testa, ed uno attuale, cioe' nod)
-Soluzione: quando trovo il nodo dico al precedente di puntare al successivo e poi libero la memoria
-*/        coda=inserisci_in_coda(coda,testa);
+        coda=inserisci_in_coda(coda,testa);
           testa=aus2; //aus2 tiene conto di tutte le modifiche eventuali della testa della lista
           return testa;
       }else{
@@ -119,7 +107,7 @@ Soluzione: quando trovo il nodo dico al precedente di puntare al successivo e po
       puts("Impossibile allocare memoria");
       exit(-1);
     }
-    strcpy(n2->indice,nod);
+    n2->indice=nod;
     n2->suc=NULL;
     
   coda=inserisci_in_coda(coda,n2);  
@@ -136,35 +124,35 @@ void stampa(struct nodo *testa){
   }
   while(testa!=NULL){
     aus=testa->suc;
-    printf("STAMPA:%s\n",testa->indice);
+    printf("STAMPA:%d\n",testa->indice);
     testa=aus;
   }
-  printf("Coda:%s\n\n",coda->indice);
+  printf("Coda:%d\n\n",coda->indice);
 }
-
-/* void main(){
+/*
+ void main(){
     struct nodo* testa;
     testa=NULL;
     coda=NULL;
     
     // Ho il solo ind1
-    char* n="ind1";
+    long n=1;
     testa=inserisci_n(testa,n);
     stampa(testa);
     
-    n="ind2";
+    n=2;
     testa=inserisci_n(testa,n);
     stampa(testa);
     
-    n="ind3";
+    n=3;
     testa=inserisci_n(testa,n);
     stampa(testa);
     
-    n="ind4";
+    n=4;
     testa=inserisci_n(testa,n);
     stampa(testa);
     
-    n="ind5";
+    n=5;
     testa=inserisci_n(testa,n);
     stampa(testa);
     
@@ -178,10 +166,7 @@ void stampa(struct nodo *testa){
     testa=libera_n(testa,n);    
     stampa(testa);
     
-<<<<<<< HEAD
     testa=libera_n(testa,n);    
     
 }
-=======
-} */
->>>>>>> master
+ */
